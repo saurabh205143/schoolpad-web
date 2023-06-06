dc1e61a56f647a242e0ef1f4f35161cc15200d02
Kashcodebrigade <kashisha@codebrigade.in>
2023-06-06 12:20:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ function App()  {
           <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />
         </Helmet>
         <>
-         
             <Routes>
               <Route path='/transport' element={<Transport />} />
               <Route path='/transport/transportRoute' element={<TransportRoute />} />
