2875d33a5a709f10a0a6f8227a4c0a410ac9fcfe
Kashcodebrigade <kashisha@codebrigade.in>
2023-06-22 12:13:05 +0530

    Route Order Drag Sort List UI Updated

--- src/components/Drag&DropSortList/List.js
+++ src/components/Drag&DropSortList/List.js
@@ -8,22 +8,39 @@ const List = {
       id: 2,
       title: "Zirakpur Route",
     },
-    // {
-    //   id: 3,
-    //   title: "Route 12",
-    // },
-    // {
-    //   id: 4,
-    //   title: "Old Panchkula Route",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Manimajra Route",
-    // },
-    // {
-    //   id: 6,
-    //   title: "Patiala phase 2",
-    // },
+    {
+      id: 3,
+      title: "Route 12",
+    },
+    {
+      id: 4,
+      title: "Old Panchkula Route",
+    },
+    {
+      id: 5,
+      title: "Manimajra Route",
+    },
+    {
+      id: 6,
+      title: "Patiala phase 2",
+    },
+    {
+      id: 7,
+      title: "Ambala City",
+    },
+    {
+      id: 8,
+      title: "Rajpura Model Town",
+    },
+    {
+      id: 9,
+      title: "Route 21",
+    },
+    {
+      id: 10,
+      title: "Sector 17, Chandigarh",
+    },
+
   ],
   getList: function () {
     return (
