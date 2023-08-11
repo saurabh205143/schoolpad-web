9a3ae2264f70668970b7ebfc225576d1ee7059a1
HarpreetR <harpreetr@codebrigade.in>
2023-08-11 17:54:47 +0530

    addstorejs
    add store js implementation

--- src/views/main/InventoryModule/ManageStore/components/AddStore.js
+++ src/views/main/InventoryModule/ManageStore/components/AddStore.js
@@ -3,33 +3,99 @@ import Modal from '../../../../../components/Modal/Modal';
 import Input from '../../../../../components/Inputs/Input';
 import { FieldContainer, ModalBodyConatiner } from '../../../TransportModule/TransportRoute/components/AddRouteStyles';
 import CustomCheckbox from '../../../../../components/Checkbox/CustomCheckbox';
-
+import axios from 'axios';
+import config from '../../../../../config';
 
 const AddStore = props => {
 
     const { show, handleClose } = props;
     const [isChecked, setIsChecked] = useState(true);
-    const [name, setName] = useState('');
+    const [storeName, setstoreName] = useState('');
+    const [storeCode, setstoreCode] = useState('');
+    const [storeDesc, setstoreDesc] = useState('');
+    const [storeManager, setstoreManager] = useState('');
     const { confirm, handleConfirm } = props;
+    const baseURL = config.baseUrl +"api/v1/inventory/store";
 
     const handleChange = () => {
         setIsChecked(!isChecked);
     };
     const handleStoreNameChange = (e) => {
-        setName(e.target.value);
+        setstoreName(e.target.value);
     };
-
+    const handleStoreCodeChange = (e) => {
+        setstoreCode(e.target.value);
+    }
+    const handleStoreDescriptionChange = (e) => {
+        setstoreDesc(e.target.value);
+    }
+    const handleStoreManagerChange = (e) => {
+        setstoreManager(e.target.value);
+    }
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "content-type",
+        "Access-Control-Allow-Credentials": "true"
+        // //'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Headers':'*',
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Methods': 'POST, PUT, PATCH, GET, DELETE, OPTIONS',
+        // 'Access-Control-Allow-Credentials': true,
+        
+    }
     const handleButtonClicked = () => {
+        console.log({ baseURL });
+        try {
+                const response = axios.post(baseURL, {
+                    storeName: storeName,
+                    // other data
+                },
+                {
+                    headers: headers
+                });
+                console.log('POST successful:', response);
+                } catch (error) {
+                if (error.response) {
+                    // The request was made, but the server responded with an error status code
+                    console.error('Server responded with an error:', error.response.data);
+                } else if (error.request) {
+                    // The request was made, but no response was received
+                    console.error('No response received:', error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an error
+                    console.error('Error setting up the request:', error.message);
+                }
+        }
+    //     axios.post(baseURL, { storeName })
+    //   .then((res) => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   });
+    //     axios
+    //   .post(baseURL, {
+    //       storeName: storeName,
+    //       storeCode: storeCode,
+    //       storeDesc: storeDesc,
+    //       storeManager: storeManager,
+    //       userId: 214,
+    //       sessionId:8
+    //   })
+    //         .then((response) => {
+    //       console.log({response});
+    //     // setPost(response.data);
+    //   });
+    //     axios
+    //   .post(baseURL, { storeName,storeCode,storeDesc,storeManager })
+    //   .then((res) => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   });
     // Do something when the button in the modal is clicked
-    console.log('Button inside modal clicked!');
+    // console.log(storeName);
     // Close the modal
     // setIsModalOpen(false);
   };
-
-    const handleSubmit = event => {
-            event.preventDefault();
-            alert('You have submitted the form.')
-        }
     return (
         <Modal
             show={show}
@@ -37,9 +103,9 @@ const AddStore = props => {
             modalHeading={'Add New Store'}
             submitText='Confirm'
             cancelText='Cancel'
-            onButtonClicked={handleButtonClicked}
+            saveAction={handleButtonClicked}
         >
-        <form onSubmit={handleSubmit}>            
+        <form>            
                 <>
                 <ModalBodyConatiner>
                 <FieldContainer>
@@ -48,7 +114,7 @@ const AddStore = props => {
                                 label={'Store Name*'}
                                 placeholder={'Enter store name'}
                                 name='store_name'
-                                value={name}
+                                value={storeName}
                                 onChange={(e) => { handleStoreNameChange(e) }}
                     />
                 </FieldContainer>
@@ -58,6 +124,8 @@ const AddStore = props => {
                         placeholder={'Enter store code'}
                         label={'Store Code*'}
                         name={'store_code'}
+                        value={storeCode}
+                        onChange={(e) => { handleStoreCodeChange(e) }}
                     />
                 </FieldContainer>
                 <FieldContainer>
@@ -66,6 +134,8 @@ const AddStore = props => {
                         placeholder={'Enter store description'}
                         label={'Store Description*'}
                         name={'store_description'}
+                        value={storeDesc}
+                        onChange={(e)=>{ handleStoreDescriptionChange(e)}}
                     />
                 </FieldContainer>
                 <FieldContainer>
@@ -74,6 +144,8 @@ const AddStore = props => {
                         placeholder={'Enter store manager'}
                         label={'Store Manager*'}
                         name={'store_manager'}
+                        value={storeManager}
+                        onChange={(e)=>{ handleStoreManagerChange(e)}}
                     />
                 </FieldContainer>
                 <CustomCheckbox
@@ -86,8 +158,7 @@ const AddStore = props => {
                 
                 
             </form>
-            </Modal>
-        
+            </Modal>       
     );
 };
 
